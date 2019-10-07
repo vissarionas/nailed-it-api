2f7249a01957f92a2b1f28306ad2ba95582e6350
vissarionas <vissarionas@gmail.com>
2019-10-07 15:27:31 +0200

    Simple CRUD

--- dbAdapter.js
+++ dbAdapter.js
@@ -2,15 +2,16 @@ const Cloudant = require('@cloudant/cloudant');
 
 const account = process.env.DB_ACCOUNT;
 const password = process.env.DB_PASSWORD;
-
 const cloudant = Cloudant({ account, password });
 
-async function asyncCall() {
-  await cloudant.db.create('alice');
-  return cloudant.use('alice').insert({ happy: true }, 'rabbit');
-}
+const customersDB = cloudant.use('customers');
 
-asyncCall().then((data) => {
+const insert = async (customerDoc) => customersDB.insert(customerDoc);
+const get = async (customerID) => customersDB.get(customerID);
+const update = async (updatedCustomerDoc) => customersDB.insert(updatedCustomerDoc);
+const destroy = async (customerID, docRevision) => customersDB.destroy(customerID, docRevision);
+
+get('customer_1').then((data) => {
   console.log(data);
 }).catch((err) => {
   console.log(err);
