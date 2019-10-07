087d3e8791c06f096ff91117fb91ccd6f42e8678
vissarionas <vissarionas@gmail.com>
2019-10-07 14:39:07 +0200

    Implement basic jwt authentication

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 
 const app = express();
+
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromExtractors([
+    ExtractJwt.fromHeader('access_token'),
+  ]),
+  secretOrKey: 'secret-key',
+}, (jwtPayload, done) => done(null, jwtPayload)));
+
+app.use(passport.initialize());
+
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', passport.authenticate('jwt', { session: false }), (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server started on http://127.0.0.1:${port}!`));
