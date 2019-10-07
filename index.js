982f2b5c0a31dc85562bc4686787d34cedbd505e
vissarionas <vissarionas@gmail.com>
2019-10-07 14:58:10 +0200

    Added dotenv

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+
 const express = require('express');
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 
 const app = express();
+const port = process.env.SERVER_PORT;
 
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromExtractors([
     ExtractJwt.fromHeader('access_token'),
   ]),
-  secretOrKey: 'secret-key',
+  secretOrKey: process.env.JWT_SECRET_KEY,
 }, (jwtPayload, done) => done(null, jwtPayload)));
 
 app.use(passport.initialize());
-
-const port = 3000;
-
 app.get('/', passport.authenticate('jwt', { session: false }), (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Server started on http://127.0.0.1:${port}!`));
+app.listen(port, () => console.log(`Server started on http://127.0.0.1:${port}`));
